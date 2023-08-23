afcd830ac0afff434e1e1299408f6a50ff905847
farkhanmaul <farkhanmaul@gmail.com>
2023-08-23 17:20:18 +0700

    commit

--- config/database.js
+++ config/database.js
@@ -2,8 +2,8 @@ const mysql = require("mysql2");
 
 // Create the pool
 const pool = mysql.createPool({
-   host: process.env.HOST,
-   user: "root",
+   host: process.env.HOST || "localhost",
+   user: process.env.USER || "root",
    database: process.env.DATABASE,
    password: process.env.PASSWORD,
 });

7b08b77178e8c4c1b8390958747c8c770bf9fdb0
farkhanmaul <farkhanmaul@gmail.com>
2023-08-23 16:46:45 +0700

    commit

--- abhimata1.sql
+++ abhimata1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 23, 2023 at 11:38 AM
+-- Generation Time: Aug 23, 2023 at 11:45 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 

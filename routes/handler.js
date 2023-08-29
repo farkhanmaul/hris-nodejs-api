5ad188afeafa25687f6f7e5b903df24793ff1a97
farkhanmaul <farkhanmaul@gmail.com>
2023-08-29 15:43:59 +0700

    minor

--- routes/handler.js
+++ routes/handler.js
@@ -6,7 +6,41 @@ const verifyToken = require("../middleware/verifytoken.js");
 const Mailgen = require("mailgen");
 const randomstring = require("randomstring");
 
-// login
+// Generate OTP
+function generateOTP() {
+   return randomstring.generate({ length: 6, charset: "numeric" });
+}
+
+function generateExpirationDate() {
+   const expirationTime = new Date(Date.now() + 3 * 30 * 24 * 60 * 60 * 1000);
+   return expirationTime;
+}
+
+// Generate a random 30-digit string token
+function generateRandomToken() {
+   const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+   let token = "";
+   for (let i = 0; i < 30; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      token += characters.charAt(randomIndex);
+   }
+   return token;
+}
+
+function formatDate(date) {
+   const options = {
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+   };
+   return date.toLocaleDateString("id-ID", options);
+}
+
+// login endpoint
 async function login(req, res) {
    const { employeeId } = req.body;
 
@@ -37,16 +71,6 @@ async function login(req, res) {
    }
 }
 
-// Generate OTP
-
-function generateOTP() {
-   return randomstring.generate({ length: 6, charset: "numeric" });
-}
-
-function generateExpirationDate() {
-   const expirationTime = new Date(Date.now() + 3 * 30 * 24 * 60 * 60 * 1000);
-   return expirationTime;
-}
 // Kirim OTP ke alamat email
 async function sendOTP(receiver, otp, expiredAt, employeeId) {
    // Add expiredAt parameter
@@ -182,30 +206,6 @@ async function loginOTP(req, res) {
       console.error("Failed to verify OTP:", error);
       response(500, "99", "Internal Server Error", {}, res);
    }
-}
-
-// Generate a random 30-digit string token
-function generateRandomToken() {
-   const characters =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-   let token = "";
-   for (let i = 0; i < 30; i++) {
-      const randomIndex = Math.floor(Math.random() * characters.length);
-      token += characters.charAt(randomIndex);
-   }
-   return token;
-}
-
-function formatDate(date) {
-   const options = {
-      day: "numeric",
-      month: "long",
-      year: "numeric",
-      hour: "numeric",
-      minute: "numeric",
-      second: "numeric",
-   };
-   return date.toLocaleDateString("id-ID", options);
 }
 
 async function userProfile(req, res) {

f4b2f6f498bba68020e355453221b2e102d64c74
farkhanmaul <farkhanmaul@gmail.com>
2023-09-08 15:32:18 +0700

    nodemon

--- routes/routes.js
+++ routes/routes.js
@@ -18,6 +18,7 @@ const {
 } = require("./handler");
 
 router.post("/login", userController.login); // user/login
+router.post("/login2", userController.login2); // user/login2
 router.post("/login-otp", userController.loginOTP); // user/login-otp
 router.post("/user", verifyToken, userController.userProfile); // user/get
 router.post("/attendance", verifyToken, userController.attendance); // user/attendance
@@ -25,7 +26,6 @@ router.post("/user-attendance", verifyToken, userController.getAttendance); // u
 router.post("/user-attendance-clock", verifyToken, userController.getClockTime); // user/get clock data
 router.post("/logout", verifyToken, userController.logout); // user/logout
 router.post("/verify-token", userController.verifyTokenHandler);
-router.post("/login2", userController.login2); // user/login2
 
 router.post("/user-attendance-history", verifyToken, getAttendanceHistory); // user/get list atendance by day
 module.exports = router;

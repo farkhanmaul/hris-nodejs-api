a538b10d14cd5d055ddaf795af13850f43137292
farkhanmaul <farkhanmaul@gmail.com>
2023-11-29 14:15:38 +0700

    fix(bug): fix bug

--- controllers/user_attendance_controller.js
+++ controllers/user_attendance_controller.js
@@ -430,14 +430,15 @@ const storage = multer.diskStorage({
 
       function formatDate(date) {
          const year = date.getFullYear();
-         const month = date.getMonth() + 1;
-         const day = date.getDate();
-         const hour = date.getHours();
-         const minute = date.getMinutes();
-         const second = date.getSeconds();
-         const formattedDate = `${year}${month}${day}${hour}${minute}${second}`;
+         const month = String(date.getMonth() + 1).padStart(2, "0");
+         const day = String(date.getDate()).padStart(2, "0");
+         const hour = String(date.getHours()).padStart(2, "0");
+         const minute = String(date.getMinutes()).padStart(2, "0");
+         const second = String(date.getSeconds()).padStart(2, "0");
+         const formattedDate = `${year}${month}${day}_${hour}${minute}${second}`;
          return formattedDate;
       }
+
       const date = new Date();
       const formattedDate = formatDate(date);
       const type = req.body.type;
